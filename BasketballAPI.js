40f69caa44a076fd68341e5befa7e35a1592f7f6
Florence Braddy <florencebraddy@florences-air.attlocal.net>
2020-04-11 20:53:02 -0400

    GET player name and team

---
+++ BasketballAPI.js
@@ -0,0 +1,31 @@
+function getRequest() {
+  var url = "https://www.balldontlie.io/api/v1/players?search=";
+  var playerName = document.getElementById("myText").value;
+  var request = new XMLHttpRequest();
+
+  //Open a new connection, using the GET request on the URL endpoint
+
+  request.open("GET", url + playerName, true);
+  request.onload = function() {
+    var data = JSON.parse(this.response);
+    if (request.status >= 200 && request.status < 400) {
+      var firstName = data.data[0].first_name;
+      var lastName = data.data[0].last_name;
+      var teamName = data.data[0].team.full_name;
+      //var division = data.data[0].team.division;
+      //var position = data.data[0].position;
+
+      var myArr = JSON.parse(this.responseText);
+
+      document.getElementById("player-name").innerHTML =
+        firstName + " " + lastName;
+      document.getElementById("team-name").innerHTML = teamName;
+      document.getElementById("division").innerHTML = division;
+      document.getElementById("position").innerHTML = position;
+    } else {
+      console.log("error");
+    }
+  };
+  // Send request
+  request.send();
+}
